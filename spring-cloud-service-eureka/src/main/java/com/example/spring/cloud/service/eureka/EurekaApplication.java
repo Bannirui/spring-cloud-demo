8ce5eeea2a0b068bbf2a1ab51936c2b9d2ec08ad
Bannirui <1652959004@qq.com>
2020-05-14 10:51:02 +0800

    eureka服务端

---
+++ spring-cloud-service-eureka/src/main/java/com/example/spring/cloud/service/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.example.spring.cloud.service.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: dingrui
+ * @Date: Create in 2020/5/14
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
